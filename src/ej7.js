4228a0973737b5e6069b433492db67fb26193c42
KevinSebaLee <48044115@est.ort.edu.ar>
2025-03-28 09:58:44 -0300

    Finalizar ej7

--- src/ej7.js
+++ src/ej7.js
@@ -1,1 +1,14 @@
-console.log('hola')
+import { getCountryByAbbreviation } from 'currency-map-country';
+import { getCurrencyAbbreviation } from 'currency-map-country';
+
+let monedaDelPais, codigoPais;
+codigoPais = 'AR';
+monedaDelPais = obtenerMoneda(codigoPais);
+console.log(`La moneda del país ${codigoPais} es: ${monedaDelPais}`);
+codigoPais = 'UZA';
+monedaDelPais = obtenerMoneda(codigoPais);
+console.log(`La moneda del país ${codigoPais} es: ${monedaDelPais}`);
+
+function obtenerMoneda(codigoPais){
+    return getCurrencyAbbreviation(getCountryByAbbreviation(codigoPais))
+}
