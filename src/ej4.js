cbd136002c73aa173571a4c7df589d957d82857b
KevinSebaLee <48044115@est.ort.edu.ar>
2025-03-28 09:35:10 -0300

    Ej4 terminado

--- src/ej4.js
+++ src/ej4.js
@@ -1,1 +1,25 @@
-console.log('hola')
+import fs from 'fs';
+const ARCHIVO_ENTRADA = "./src/archivosTXT/archivo-entrada.txt";
+const ARCHIVO_SALIDA = "./src/archivosTXT/archivo-salida.txt";
+console.clear();
+copiar(ARCHIVO_ENTRADA, ARCHIVO_SALIDA);
+
+function copiar(origen, destino){
+    fs.copyFile(origen, destino, (err) => {
+        if (err) {
+            console.log("Error Found:", err);
+        }
+        else {
+            getCurrentFilenames();
+            console.log("\nFile Contents of copied_file:",
+                fs.readFileSync(`${destino}`, "utf8"));
+        }
+    });
+}
+
+function getCurrentFilenames() {
+    console.log("\nCurrent filenames:");
+    fs.readdirSync(__dirname).forEach(file => {
+        console.log(file);
+    });
+}
