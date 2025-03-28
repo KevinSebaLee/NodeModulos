dff8078bdf19354bc7c6af72d38c3c2a6adaffa4
KevinSebaLee <48044115@est.ort.edu.ar>
2025-03-28 09:47:52 -0300

    Finalizar ej 5

--- src/ej5.js
+++ src/ej5.js
@@ -1,1 +1,19 @@
-console.log('hola')
+import { URL } from 'node:url';
+
+let miUrl = null;
+let miObjeto = null;
+miUrl = 'http://www.ort.edu.ar:8080/alumnos/index.htm?curso=2022&mes=mayo';
+miObjeto = parsearUrl(miUrl);
+console.log(miObjeto);
+
+function parsearUrl(laURL) {
+    const myURL = new URL(laURL);
+    
+    const URLObject = {
+        puerto: myURL.port,
+        pathname: myURL.pathname,
+        parameters: myURL.searchParams
+    };
+
+    return URLObject;
+}
