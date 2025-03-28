68538abd11666e9400b2cb47f5bba8b08c84decc
KevinSebaLee <48044115@est.ort.edu.ar>
2025-03-28 08:51:55 -0300

    Finazliar ej2

--- src/ej2.js
+++ src/ej2.js
@@ -1,1 +1,16 @@
-console.log('hola')
+import {PI, sumar, dividir, multiplicar, restar, numeros} from './modules/matematica.js'
+
+let numero1=10, numero2=20;
+console.clear();
+console.log(`La constante PI vale '${PI}'`);
+console.log(`Los numeros importados son '${numeros}`)
+
+let Sumar = sumar(numero1, numero2); 
+let Restar = restar(numero1, numero2); 
+let Dividir = dividir(numero1, numero2); 
+let Multiplicar = multiplicar(numero1, numero2); 
+
+console.log(`sumar(${numero1}, ${numero2}) = ${Sumar}`);
+console.log(`restar(${numero1}, ${numero2}) = ${Restar}`);
+console.log(`dividir(${numero1}, ${numero2}) = ${Dividir}`);
+console.log(`multiplicar(${numero1}, ${numero2}) = ${Multiplicar}`);
---
+++ src/modules/matematica.js
@@ -0,0 +1,17 @@
+const PI = 3.14;
+const numeros = ["dos", "cuatro", "ocho", "diez"]
+
+const sumar = (x, y) => {
+    return x+y;
+}
+const multiplicar = (x, y) => {
+    return x*y;
+};
+const restar = (x, y) => {
+    return x-y;
+}
+const dividir = (x, y) => {
+    return x/y;
+}
+
+export {PI, sumar, multiplicar, restar, dividir, numeros};
